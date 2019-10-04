b9712a67353d15076abee8d5b2518367399e6005
Tanya Bihag <tbihag@gmail.com>
2019-10-04 02:43:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,21 +1,8 @@
 class Song 
-@@all = []
-attr_accessor :name, :artist 
-
+  attr_accessor :name
+  @@all = []
+  
 def initialize(name)
-  @name = name 
-  @artist = artist 
-end 
-
-def self.all 
-  @all
-end 
-
-def self.new_by_filename
-  artist = filename.split(" - ")[0] 
-  song = filename.split(" - ")[1]
-  song = Song.new(song)
-  song.artist_name = artist 
-  song
-end
-end
+  @name = name
+  save
+  
