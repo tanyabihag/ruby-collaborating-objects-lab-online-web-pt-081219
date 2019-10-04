5f181567958db3c4b9096e8c47e245edb8193fd8
Tanya Bihag <tbihag@gmail.com>
2019-10-04 02:53:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,5 +19,9 @@ def artist_name=(artist_name)
   self.artist = Artist.find_or_create_by_name(artist.name)
 
 def self.new_by_filename(filename)
-  artist = 
+  artist = filename.split(" - ")[0]
+  tack = filename.split(" - ")[1]
+  song = Song.new(track)
+  song.artist_name = artist
+  song
 end 
