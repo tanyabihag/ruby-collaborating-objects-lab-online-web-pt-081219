6d40995570452f3ed10cab3eab6f61f4ddb50635
Tanya Bihag <tbihag@gmail.com>
2019-10-04 02:10:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,9 +15,22 @@ def songs
   end 
 end
 
+def add_song(song)
+  song.artist = self
+end 
+
 def self.all
   @@all
 end 
+
+def self.find_or_create_by_name(name)
+  Artist.name(name)
+  if Artist.all.find |artist|
+    artist.name ==name
+  end
+  if result
+
+end
 
 
 end
